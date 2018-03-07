65c6b321cc4cffe9233086fcf0d0fc40ef5762ae
SebastianH11 <36234258+SebastianH11@users.noreply.github.com>
2018-03-07 10:07:21 -0500

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,22 +22,55 @@ namespace u2_j1_QuadrantsStuff_Sebastian
          
     {
         string Quadrants;
-
+        int X_int = 69;
+        int Y_int = 69;
         public MainWindow()
         {
-            Quadrants = "i want to die";
+            Quadrants = "";
             Console.WriteLine(Quadrants);
             InitializeComponent();
         }
-       
+
         private void ButtonTest_Click(object sender, RoutedEventArgs e)
 
         {
+
             Quadrants = txtbxTest.Text;
+            int.TryParse(txtbxTest.Text.Substring(0, Quadrants.IndexOf('\r')), out X_int);
+
+            Console.WriteLine(X_int.ToString());
+
+            int.TryParse(txtbxTest.Text.Substring(Quadrants.IndexOf('\r') + 2, Quadrants.Length - Quadrants.IndexOf('\r') - 2), out Y_int);
+
+            Console.WriteLine(Y_int.ToString());
+
+            if (X_int > 0)
+            {
+                if (Y_int > 0)
+                {
+                    lblTest.Content = "Quadrant 1";
+                }
+            }
+            else if (X_int < 0)
+            {
+                if (Y_int > 0)
+                {
+                    lblTest.Content = "Quadrant 2";
+                }
+            }
+            else if (X_int < 0 || Y_int < 0)
+            {
+                lblTest.Content = "Quadrant 3";
+            }
+            else if (X_int > 0 || Y_int < 0)
+            {
+                lblTest.Content = "Quadrant 4";
+            }
+            /*  Quadrants = txtbxTest.Text;
             if (Quadrants == "hi")
             {
                 lblTest.Content = "YAY";
-            }
+            }*/
 
         
         } 
